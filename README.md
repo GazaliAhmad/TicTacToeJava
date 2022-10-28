93e9fadcfad06fb799dc2d752c60c807a2d795f7
Gazali <gazali.ahmad@gmail.com>
2022-10-28 18:00:16 +0800

    Fixed Cat and some updates

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # TicTacToe
 
 A simple TicTacToe game written in Java.
-
-## Stretch Goal
-- cpu moves based on strategy and not randomly generted. Even with random moves, cpu sometimes can win.
-
-## Super Stretch Goal
-- Convert it to GUI-based game.
--- src/com/journeyman/TicTacToe.java
+++ src/com/journeyman/TicTacToe.java
@@ -1,20 +1,15 @@
 package com.journeyman;
 
+import org.jetbrains.annotations.NotNull;
 import java.util.*;
+import static java.lang.System.err;
+import static java.lang.System.out;
+import static java.text.MessageFormat.format;
 
-/* A simple TicTacToe game
-
-Stretch Goal:
-1. Convert cpu placement from random to strategy rules. Even if it's random, cpu sometimes can win.
-
-Super Stretch Goal:
-1. Convert it to a Windows GUI game.
-
- */
-
+@SuppressWarnings("ReassignedVariable")
 public class TicTacToe {
-    static ArrayList<Int7eger> playerPositions = new ArrayList<Integer>();
-    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+    final static ArrayList<Integer> playerPositions = new ArrayList<>();
+    final static ArrayList<Integer> cpuPositions = new ArrayList<>();
 
     public static void main(String[] args) {
         char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
@@ -26,13 +21,13 @@ public class TicTacToe {
         printGameBoard(gameBoard);
 
         while (true) {
-            Scanner scan = new Scanner(System.in);
-            System.out.printf("\nEnter your placement (1 - 9): ");
+            var scan = new Scanner(System.in);
+            out.print("\nEnter your placement (1 - 9): ");
 
-            int playerPos = scan.nextInt();
+            var playerPos = scan.nextInt();
 
             while (playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)) {
-                System.out.println("Position taken! Enter a correct position: ");
+                err.println("Position taken! Enter a correct position: ");
                 playerPos = scan.nextInt();
             }
             placePiece(gameBoard, playerPos, "player");
@@ -41,7 +36,7 @@ public class TicTacToe {
 
             String result = checkWinner();
             if (result.length() > 0) {
-                System.out.println(result);
+                out.println(result);
                 break;
             }
 
@@ -52,29 +47,29 @@ public class TicTacToe {
                 cpuPos = rand.nextInt(9) + 1;
             }
             placePiece(gameBoard, cpuPos, "cpu");
-
-            System.out.printf("\ncpu placement at position: %d%n", cpuPos);
+            
+            out.println(format("\nCPU placed at position: {0}", cpuPos));
 
             printGameBoard(gameBoard);
 
             result = checkWinner();
             if (result.length() > 0) {
-                System.out.println(result);
+                out.println(result);
                 break;
             }
         }
     }
 
-    public static void printGameBoard(char[][] gameBoard) {
+    public static void printGameBoard(char @NotNull [][] gameBoard) {
         for (char[] row : gameBoard) {
             for (char c : row) {
-                System.out.print(c);
+                out.print(c);
             }
-            System.out.println();
+            out.println();
         }
     }
 
-    public static void placePiece(char[][] gameBoard, int pos, String user) {
+    public static void placePiece(char[][] gameBoard, int pos, @NotNull String user) {
         char symbol = ' ';
 
         if (user.equals("player")) {
@@ -98,50 +93,42 @@ public class TicTacToe {
         }
     }
 
-    public static String checkWinner() {
+    @SuppressWarnings({"rawtypes", "SuspiciousMethodCalls"})
+    public static @NotNull String checkWinner() {
 
-        List topRow = Arrays.asList(1, 2, 3);
-        List midRow = Arrays.asList(4, 5, 6);
-        List botRow = Arrays.asList(7, 8, 9);
+        List<Integer> topRow = Arrays.asList(1, 2, 3);
+        List<Integer> midRow = Arrays.asList(4, 5, 6);
+        List<Integer> botRow = Arrays.asList(7, 8, 9);
 
-        List lefCol = Arrays.asList(1, 4, 7);
-        List midCol = Arrays.asList(2, 5, 8);
-        List rigCol = Arrays.asList(3, 6, 9);
+        List<Integer> lefCol = Arrays.asList(1, 4, 7);
+        List<Integer> midCol = Arrays.asList(2, 5, 8);
+        List<Integer> rigCol = Arrays.asList(3, 6, 9);
 
-        List cross1 = Arrays.asList(1, 5, 9);
-        List cross2 = Arrays.asList(3, 5, 7);
+        List<Integer> cross1 = Arrays.asList(1, 5, 9);
+        List<Integer> cross2 = Arrays.asList(3, 5, 7);
 
-        List<List> winning = new ArrayList<List>();
+        List<List> winning = new ArrayList<>();
         winning.add(topRow);
         winning.add(midRow);
         winning.add(botRow);
-
+        
         winning.add(lefCol);
         winning.add(midCol);
         winning.add(rigCol);
-
+        
         winning.add(cross1);
         winning.add(cross2);
-
-        /* Logic Error
-
-        Last Pos when it's clearly a tie, program still runs and don't stop
-
-         */
-
-        for (int i = 0; i < winning.size(); i++) {
-            List l = winning.get(i);
+        
+        for (List l : winning) {
             if (playerPositions.containsAll(l)) {
-                return "Congratulations you won !!";
+                return "Congratulations you won!";
             } else if (cpuPositions.containsAll(l)) {
-                return "The CPU won !!";
-            } else if (winning.size() == 9){
-                return "It's a tie";
-
-                //else if (playerPositions.size() + cpuPositions.size() == 9) {
-                //    return "It's a tie !!";
+                return "CPU wins!!!";
+            } else if (playerPositions.size() + cpuPositions.size() == 9) {
+                return "CAT!";
             }
         }
+    
         return "";
     }
 }
